02461a6ddcd0aec321fe960f68381e61bdf848ab
SuhaniRana <70816383+SuhaniRana@users.noreply.github.com>
2021-04-22 00:34:43 -0400

    Add files via upload

---
+++ amazonData.py
@@ -0,0 +1,6 @@
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+df = pd.read_csv("amazonData.csv")
+fig = ff.create_distplot([df["Avg Rating"].tolist()],["Avg Rating"],show_hist=True)
+fig.show()
